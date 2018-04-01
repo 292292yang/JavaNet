dbfa0a9e44ab3d685f9513e5dca0e4c55473b9a5
zhou <wenchaozhou@qq.com>
2018-04-01 17:03:15 +0800

    java net almost over

--- src/com/URL/TestURL.java
+++ src/com/URL/TestURL.java
@@ -2,7 +2,6 @@ package com.URL;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
@@ -10,9 +9,8 @@ import java.net.URLConnection;
 public class TestURL {
     public static void main(String[] args) {
         try{
-            URL url = new URL("http://ipgw.neu.edu.cn/srun_portal_pc.php?ac_id=1&");
+            URL url = new URL("http://www.baidu.com");
             URLConnection conn = url.openConnection();
-            System.out.println(conn);
             BufferedReader bufin = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String data = null;
             while((data=bufin.readLine())!=null){
